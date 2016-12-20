d7a4c505e2e2f3ab3c13bc65219880e89b66631f
Freek Alleman <freek.alleman@raaftech.com>
2016-12-20 23:20:11 +0100

    Unrolled the for loop.

--- scrabble.js
+++ scrabble.js
@@ -1,36 +1,27 @@
-var R = require('ramda');
-
-// TODO clean up and use Ramda
 var scrabble = function(steentjes, zoekwoord) {
 
-    // hou een teller bij voor elke unieke letter
+    // maak een hash met teller voor elke unieke letter
     var telLetters = woord => woord.split('').reduce((hash,elt) => hash[elt] ?
             (hash[elt]++,hash) :
             (hash[elt] = 1,hash),{});
 
-    t = telLetters(steentjes); // [ 'a', 'a', 'b' ] => { 'a': 2, 'b': 1}
-    w = telLetters(zoekwoord); // [ 'a', 'b', 'b' ] => { 'a': 1, 'b': 2}
+    t = telLetters(steentjes);
+    w = telLetters(zoekwoord);
 
-    // kijk voor elke unieke letter of deze vaak genoeg voorkomt op je plankje
-    for(letter in w) {
+    var enough = letter => t[letter]>=w[letter];
+    var boyAND = (x,y)  => x && y;  // geleend van boy ;)
 
-        // hoeveel keer hebben we deze letter nodig?
-        aantalNodig = w[letter];
-
-        // indien we niet voldoende van deze letter hebben op ons plankje, exit
-        if(R.isNil(t[letter]) || t[letter]<aantalNodig){
-            return false;
-        }
-    }
-
-    // blijkbaar alle letters gevonden
-    return true;
+    return zoekwoord.split('').map(enough).reduce(boyAND);
 }
 
-console.log(scrabble("ladilmy", "daily"));   // -> true
-console.log(scrabble("eerriin", "eerie"));   // -> false
-console.log(scrabble("orrpgma", "program")); // -> true
-console.log(scrabble("orppgma", "program")); // -> false
+console.log(scrabble('wfboaor', 'foo'));
+console.log(scrabble('foboar', 'fooz'));
+
+//console.log(scrabble("ladilmy", "daily"));   // -> true
+//console.log(scrabble("ladilmy", "zwedaily"));// -> false
+//console.log(scrabble("eerriin", "eerie"));   // -> false
+//console.log(scrabble("orrpgma", "program")); // -> true
+//console.log(scrabble("orppgma", "program")); // -> false
 
 // TODO bonus 1 (blanco steentjes)
 /*
